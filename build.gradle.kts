33b7d7b670a034e5a54ee8e68422a8b94b7c96ea
Raul Q.M <raulquesadamorera@gmail.com>
2025-12-09 19:49:24 -0600

    Agregado comentario explicativo en build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,6 @@
+// Este es el archivo de configuración de nivel superior para el proyecto
+// Aquí se pueden agregar configuraciones comunes para todos los subproyectos o módulos.
+
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
     alias(libs.plugins.android.application) apply false
